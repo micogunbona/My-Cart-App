25f0b06cb290d96c88dca74589adc0b813d304bb
Micheal Ogunbona <110333583+micogunbona@users.noreply.github.com>
2023-07-06 18:44:37 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import { getDatabase, ref, push, onValue } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 const appSettings = {
     databaseURL: "https://realtime-database-1bbf0-default-rtdb.europe-west1.firebasedatabase.app/"
